716eb85244d0a6c62c38352df53d231d3253e745
Murilo Felpeto <murilofelpeto@gmail.com>
2019-07-27 20:10:53 -0300

    Editing controller

--- src/main/java/estoque/ferragens/controller/ProdutoController.java
+++ src/main/java/estoque/ferragens/controller/ProdutoController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,10 +32,21 @@ public class ProdutoController {
 	@GetMapping("/categoria")
 	public ResponseEntity<List<Produto>> getByCategoria(
 			@RequestParam(value = "categoria", required = true) String categoria) {
-		// TODO Tratamento de erro
 
 		List<Produto> produtos = produtoRepo.findByCategoriaNomeCategoria(categoria);
 		return (produtos.isEmpty()) ? new ResponseEntity<>(HttpStatus.NO_CONTENT)
 				: new ResponseEntity<>(produtos, HttpStatus.OK);
+	}
+	
+	@PostMapping()
+	public ResponseEntity<Produto> postProduto(@RequestBody Produto requestBody) {
+		Produto produto = new Produto();
+		produto.setCategoria(requestBody.getCategoria());
+		produto.setDescricao(requestBody.getDescricao());
+		produto.setNomeProduto(requestBody.getNomeProduto());
+		produto.setSku(requestBody.getSku());
+		produto.setUnidade(requestBody.getUnidade());
+		produtoRepo.save(requestBody);
+		return new ResponseEntity<Produto>(HttpStatus.CREATED);
 	}
 }
